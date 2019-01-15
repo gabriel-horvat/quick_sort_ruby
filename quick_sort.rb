6e93588bb2e3e790056a6366a27fbb91f62b536a
Gabe Horvat <realgabrielhorvat@gmail.com>
2019-01-15 12:11:06 +0100

    implement the full algorithm using recursion

--- quick_sort.rb
+++ quick_sort.rb
@@ -1,7 +1,21 @@
-def quick_sort(array)
+def partition(operator, array)
 
+  array.select { |n| n.send(operator, array.first) }
+  #            { |n| n < array.sample }
+  # select everything that is smaller than array.sample, which is the pivot itself, and put it into left side of pivot
 end
 
-# test method
+def quick_sort(array)
+  return [] if array.empty?
+  quick_sort(partition(:<, array)) + [array.first] + quick_sort(partition(:>, array))
+  # 1) pick random pivot element
+  # 2) partition array into 3 elements
+  #       left side of pivot < pivot
+  #       pivot itself
+  #       right side of pivot > pivot
+  # keep doing A and B until array fully sorted
+end
+
 a = [5, 10, 12, 3, 1, 7]
-quick_sort(a)
+
+p quick_sort(a)
